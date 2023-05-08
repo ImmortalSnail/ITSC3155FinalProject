01ec9ac292f37a81cac40f90f57a3d2c770d29ac
ImmortalSnail <teddybrosevelt8@gmail.com>
2023-05-07 20:37:39 -0400

    added config file

---
+++ config_example.py
@@ -0,0 +1,11 @@
+import secrets
+
+DB_USERNAME = 'name'
+DB_PASSWORD = 'password'
+DB_HOST = 'hostname'
+DB_PORT = 1111
+DB_NAME = 'dbname'
+SALT = secrets.token_hex(1)
+SECRET = secrets.token_hex(1)
+
+# Fill out with appropriate information
